b42ba6f736bf67a8d173638e5dcaf5b6799094dd
Tiago Castro <tiagocastro602@gmail.com>
2022-04-18 20:05:57 -0300

    Correção na estrutura de arquivos e título da página

--- eplus-challenge/src/pages/_document.js
+++ eplus-challenge/src/pages/_document.js
@@ -5,11 +5,14 @@ export default function Document() {
     <Html>
       <Head lang="pt-br">
         <link rel="icon" type="image/png" href="./images/logo/favicon.png" />
-        <title>Desafio E-Plus</title>
 
         {/* Import the Montserrat font family */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
           rel="stylesheet"
--- eplus-challenge/src/pages/index.js
+++ eplus-challenge/src/pages/index.js
@@ -1,3 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello E-Plus!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Desafio E-Plus</title>
+      </Head>
+
+      <h1>Hello E-Plus!</h1>
+    </>
+  );
 }
